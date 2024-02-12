6d7eabb78b94906a45ab7c410f71fc6205412a6a
andrewstephengames <andrewstephengames999@gmail.com>
2024-02-12 15:20:02 +0200

    Testing StringSet and StringCat.

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
+#include <stdio.h>
 #include "String.h"
 
 int main (void) {
-     String s;
-     s = StringInit ("test", 4);
-     printf ("%c\n", s.data[0]);
+     String s1, s2;
+     s1 = StringSet ("Hello");
+     printf ("%s: %d\n", s1.data, s1.size);
+     s2 = StringSet ("World");
+     printf ("%s: %d\n", s2.data, s2.size);
+     s1 = StringCat (s1, space);
+     s1 = StringCat (s1, s2);
+     printf ("%s: %d\n", s1.data, s1.size);
+     free (s1.data);
      return 0;
 }
