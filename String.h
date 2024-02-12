9ed5e91c124c31fc7f80d5f49fa3572c0268f154
andrewstephengames <andrewstephengames999@gmail.com>
2024-02-12 15:19:06 +0200

    String.h

--- String.h
+++ String.h
@@ -1,56 +1,84 @@
 #ifndef STRING_H_
 #define STRING_H_
 
-#include <stdlib.h>
-#include <stdio.h>
-
 #ifndef STRDEF
 #define STRDEF
 #endif // STRDEF
+
+#include <stdlib.h>
+#include <assert.h>
+
+#define bool int
+#define true 1
+#define false 0
+#define space (String) { .data = " ", .size = 1 }
 
 typedef struct {
      char *data;
      size_t size; 
 } String;
 
-STRDEF String StringInit (char *data, size_t size);
-STRDEF void StringFree (String *s);
-STRDEF String StringToUpper (String *s);
+STRDEF String StringSet (char *data);
+STRDEF static char *Overwrite (char *d, char *s);
+STRDEF char *StringToC (String s);
+STRDEF String StringCat (String s1, String s2);
+STRDEF bool StringEquals (String s1, String s2);
+STRDEF String CharToString (char c);
 
-STRDEF String StringInit (char *data) {
+STRDEF String StringSet (char *data) {
      String s;
-     for (int i = 0; data[i] != 0; ++i)
-     s.data = malloc (s.size * sizeof (char));
+     size_t size = 0;
+     for (int i = 0; data[i] != 0; ++i) {
+          size++;
+     }
+     s.data = data;
+     s.size = size;
      return s;
 }
 
-STRDEF void StringFree (String *s) {
-     free (s->data);
-     s->data = NULL;
-     s->size = 0;
+STRDEF static char *Overwrite (char *d, char *s) {
+     if (d == NULL) {
+          d = "";
+     }
+     if (s == NULL) {
+          s = "";
+     }
+     while ((*d = *s)) {
+          ++s;
+          ++d;
+     }
+     return d;
 }
 
-STRDEF String StringToUpper (String *s) {
-     if (s->size == 0) {
-          fprintf (stderr, "Give me a workable String!");
-          exit(1);
+STRDEF char *StringToC (String s) {
+     char *data = s.data;
+     if (data == NULL) {
+          data = "";
      }
-     for (int i = 0; i < s->size; ++i) {
-          if (s->data[i] >= 'a' && s->data[i] <= 'z')
-               s->data[i] -= 32;
-     }
-     return *s;
+     return data;
 }
-STRDEF String StringToLower (String *s) {
-     if (s->size == 0) {
-          fprintf (stderr, "Give me a workable String!");
-          exit(1);
-     }
-     for (int i = 0; i < s->size; ++i) {
-          if (s->data[i] >= 'A' && s->data[i] <= 'Z')
-               s->data[i] += 32;
-     }
-     return *s;
+
+STRDEF String StringCat (String s1, String s2) {
+     String s;
+     size_t size = s1.size + s2.size;
+     s.data = malloc (size * 2 + 1);
+     s.size = size;
+     assert (s.data != NULL && "Buy more RAM lol");
+     Overwrite (s.data, s1.data);
+     Overwrite (s.data + s1.size, s2.data);
+     return s;
+}
+
+STRDEF bool StringEquals (String s1, String s2) {
+     return s1.data == s2.data;
+}
+
+STRDEF String CharToString (char c) {
+     String s, t;
+     s.size = 1;
+     s.data = malloc (2 * sizeof(char));
+     s.data[0] = c;
+     return s;
 }
 
 #endif // STRING_H_
